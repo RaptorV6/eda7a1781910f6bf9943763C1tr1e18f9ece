05e467d7b43511676e461e7d7bbad2166da7aac8
RaptorV6 <142246142+RaptorV6@users.noreply.github.com>
2026-05-06 10:53:02 +0000

    aha

--- PortalComponent/obj/Release/net10.0/PortalComponent.AssemblyInfo.cs
+++ PortalComponent/obj/Release/net10.0/PortalComponent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalComponent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c470fa22a913633668df3fec2870eeee8d79b0a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5cce46f6fc41fdfb687606ce6a36f3730c1fc4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalComponent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalComponent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
